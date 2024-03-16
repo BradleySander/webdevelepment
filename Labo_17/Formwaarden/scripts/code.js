7979a00023cd408b8c06c55edcbfa8de27bf76a1
BradleySander <bradley.sander@student.vives.be>
2024-03-16 16:41:24 +0100

    Labo 17 fixes

--- Labo_17/Formwaarden/scripts/code.js
+++ Labo_17/Formwaarden/scripts/code.js
@@ -10,17 +10,33 @@ const setup = () => {
         const isSmoker = result['smoker'] ? 'is roker' : 'is geen roker';
         const language = `moedertaal is ${result['language']}`;
         const neighbor = `favoriete buurland is ${result['neighbor']}`;
-        const order = `bestelling bestaat uit ${result['order'] || 'geen'}`;
+        const order = `bestelling bestaat uit ${result['order'] ? result['order'] : 'geen'}`;
 
         console.log(isSmoker);
         console.log(language);
         console.log(neighbor);
         console.log(order);
-
-        console.log(result['smoker'] ? "is well been roker" : "is geen roker");
     }
 
-    document.getElementById("showResultButton").addEventListener("click", showResult);
+    document.getElementById("showResultButton").addEventListener("click", () => {
+        const languageRadioButtons = document.getElementsByName("language");
+        let languageSelected = false;
+        for (const radioButton of languageRadioButtons) {
+            if (radioButton.checked) {
+                languageSelected = true;
+                break;
+            }
+        }
+
+        const orderSelect = document.getElementById("order");
+        const orderSelected = orderSelect.selectedOptions.length > 0;
+
+        if (languageSelected && orderSelected) {
+            showResult();
+        } else {
+            alert("Selecteer ten minste één moedertaal en één bestelling.");
+        }
+    });
 }
 
 window.addEventListener("load", setup);
